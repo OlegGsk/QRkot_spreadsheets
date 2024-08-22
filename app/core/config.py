78ba7081124f41a6774a62aa7f06a9cedc0c5146
Олег Говоровский <oleq.gov@ya.ru>
2024-08-22 22:28:13 +0500

    postgr2

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,8 @@ from pydantic import BaseSettings, EmailStr
 class Settings(BaseSettings):
     app_title: str = 'Фонд поддержки котиков'
     description: str = 'Фонд собирает пожертвования'
-    database_url: str = 'sqlite+aiosqlite:///./charitable_foundation.db'
+    # database_url: str = 'sqlite+aiosqlite:///./charitable_foundation.db'
+    database_url: str = 'postgresql+asyncpg://postgres:89090028546@localhost:5432/charitable_foundation'
     secret: str = 'SECRET'
     first_superuser_email: Optional[EmailStr] = None
     first_superuser_password: Optional[str] = None
